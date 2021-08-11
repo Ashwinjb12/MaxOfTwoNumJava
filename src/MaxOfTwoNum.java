3028e22512d9dc8e61e016cae2229d2d370584b9
ashwin <ashwinjb2@gmail.com>
2021-08-11 13:14:42 +0530

    first commit

---
+++ src/MaxOfTwoNum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MaxOfTwoNum {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the First number -");
+		int num1 = sc.nextInt();
+		System.out.println("Enter the Second number -");
+		int num2 = sc.nextInt();
+		if(num1 > num2) {
+			System.out.println("num 1 is greater");
+		} else {
+			System.out.println("num 2 is greater");
+		}
+			
+			
+	}
+}
